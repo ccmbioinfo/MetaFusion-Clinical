1105d619fd4a2baf789afeba04b2042535e39d6b
Michael Apostolides <maposto@michaels-mbp.home>
2023-10-03 18:34:47 -0400

    added VIEW_database_tables.sh

---
+++ scripts/VIEW_database_tables.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script to update "clinical_fusions" or "false_positives" tables
+
+# Absolute path to database 
+database=/Users/maposto/MetaFusion-Clinical/MWE/historical_database.db
+
+# Absolute path to excel spreadsheet with selected calls 
+# Only needed if doing "add" operation. Can use placeholder path for "view"
+#excel=/Users/maposto/MetaFusion-Clinical/MWE/run1.out/final.n2.cluster.filt.xlsx
+excel=/placeholder/file/path/nothing.xlsx
+
+# Operation "add", "view" 
+#operation=add
+operation=view
+
+# Table to view or update database tables 
+table=clinical_fusions
+#table=false_positives
+#table=historical_fusions
+
+# Run update script
+scripts=/Users/maposto/MetaFusion-Clinical/scripts
+cd  /Users/maposto/MetaFusion-Clinical/MWE
+
+Rscript $scripts/update_fusion_database_tables.R --excel=$excel --database=$database --table=$table --operation=$operation
+
+cd /Users/maposto/MetaFusion-Clinical/scripts
